1d4f8c6e80771ff8c5a79b07d08c68394b6cd5f9
rjazanovegor <rjazanovegor@mail.ru>
2021-05-15 15:38:41 +0900

    11 commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -13,7 +13,6 @@ def index(request, category_slug=None):
         products = products.filter(category=category)
     cart_product_form = CartAddProductForm()
     return render(request, 'mainapp/index.html', {
-                      'category': category,
                       'categories': categories,
                       'products': products,
                       'cart_product_form': cart_product_form,
